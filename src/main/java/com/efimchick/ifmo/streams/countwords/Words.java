e6bc5d6c5d443ef5d257f5c638fc65418933493a
thejerome <thejerome@mail.ru>
2020-04-16 11:17:03 +0300

    Streams count words initial commit

---
+++ src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
@@ -0,0 +1,12 @@
+package com.efimchick.ifmo.streams.countwords;
+
+
+import java.util.List;
+
+public class Words {
+
+    public String countWords(List<String> lines) {
+
+        return null;
+    }
+}
